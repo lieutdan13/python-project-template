ece540012304ec8fab54f260a637d0af231f914c
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2023-05-26 19:25:19 +0200

    feat: copier initializes git repository
    remote repository is also added

--- test/test_template.py
+++ test/test_template.py
@@ -7,7 +7,7 @@ from copier import run_auto
 template_variables = {var for var in yaml.safe_load(Path(__file__).parent.with_name('copier.yaml').read_text()) if not var.startswith('_')}
 print(template_variables)
 
-fp_template = Path(__file__).parent.parent/"template"
+fp_template = Path(__file__).parent.parent
 
 
 @pytest.mark.parametrize("project_name", ["My Project"])
@@ -17,7 +17,7 @@ def test_template_generation(tmp_path: Path, project_name: str, use_precommit: b
     run_auto(
         str(fp_template),
         str(tmp_path),
-        data=dict(project_name=project_name, use_precommit=use_precommit, use_bumpversion=use_bumpversion),
+        data=dict(project_name=project_name, package_name=project_name.lower().replace(" ", "_"), use_precommit=use_precommit, use_bumpversion=use_bumpversion),
     )
     fp_readme = tmp_path / 'README.md'
     assert fp_readme.is_file(), "all new projects should have a README"
@@ -31,3 +31,10 @@ def test_template_generation(tmp_path: Path, project_name: str, use_precommit: b
 
     fp_bumpversion_config = tmp_path / '.bumpversion.cfg'
     assert fp_bumpversion_config.is_file() == use_bumpversion
+
+    fp_git = tmp_path / '.git'
+    assert fp_git.is_dir(), "new projects should be git repositories"
+
+    fp_git_config = fp_git / 'config'
+    git_config = fp_git_config.read_text()
+    assert '[remote "origin"]' in git_config, "new projects should have a remote repository configured"
