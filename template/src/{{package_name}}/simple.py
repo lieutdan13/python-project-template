ec75eeab7e1983a9e644fd37ed4d601c8c3e3657
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2023-05-31 12:53:34 +0200

    fix: build docs in ci

--- template/src/{{package_name}}/simple.py
+++ template/src/{{package_name}}/simple.py
@@ -1,2 +1,10 @@
-def add_one(number):
+def add_one(number: int|float) -> int|float:
+    """Increment *number* by one.
+
+    Args:
+        number: The number to increment.
+
+    Returns:
+        Increment of *number*.
+    """
     return number + 1
