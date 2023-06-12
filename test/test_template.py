79b4cc56eca8e61806acad49358a7ab6712cddf3
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2023-06-12 19:53:35 +0200

    test: extend template tests
    install into venv
    run tests in template

--- test/test_template.py
+++ test/test_template.py
@@ -1,43 +1,93 @@
+import os
 from pathlib import Path
+from subprocess import check_output
 
 import pytest
 import yaml
 from copier import run_auto
+from pytest_venv import VirtualEnvironment
 
-template_variables = {var for var in yaml.safe_load(Path(__file__).parent.with_name('copier.yaml').read_text()) if not var.startswith('_')}
-print(template_variables)
+template_variables = {
+    var
+    for var in yaml.safe_load(
+        Path(__file__).parent.with_name("copier.yaml").read_text()
+    )
+    if not var.startswith("_")
+}
 
 fp_template = Path(__file__).parent.parent
 
+static_data = dict(
+    user_name="mkj", remote_url="git@git01.iis.fhg.de:mkj/sample-project-full.git"
+)
 
-@pytest.mark.parametrize("project_name", ["My Project"])
-@pytest.mark.parametrize("use_precommit", [True, False])
-@pytest.mark.parametrize("use_bumpversion", [True, False])
-def test_template_generation(tmp_path: Path, project_name: str, use_precommit: bool, use_bumpversion: bool):
+
+@pytest.fixture
+def venv(tmp_path):
+    """Create virtual environment in subdirectory of tmp_path."""
+    venv = VirtualEnvironment(tmp_path / ".venv")
+    venv.create()
+    (venv.path / ".gitignore").unlink()
+    yield venv
+
+
+# @pytest.mark.parametrize("project_name", ["Sample Project"])
+@pytest.mark.parametrize("use_precommit",[True, False],ids=["pre-commit", "no pre-commit"])
+@pytest.mark.parametrize("use_bumpversion", [True, False], ids=["bumpversion", "no bumpversion"])
+def test_template_generation(
+    venv: VirtualEnvironment,
+    tmp_path: Path,
+    use_precommit: bool,
+    use_bumpversion: bool,
+    project_name: str = "Sample Project",
+):
     run_auto(
         str(fp_template),
         str(tmp_path),
-        data=dict(project_name=project_name, package_name=project_name.lower().replace(" ", "_"), use_precommit=use_precommit, use_bumpversion=use_bumpversion),
+        data=dict(
+            project_name="Sample Project",
+            package_name=project_name.lower().replace(" ", "_"),
+            use_precommit=use_precommit,
+            use_bumpversion=use_bumpversion,
+            **static_data,
+        ),
     )
 
-    fp_readme = tmp_path / 'README.md'
+    fp_readme = tmp_path / "README.md"
     assert fp_readme.is_file(), "new projects should have a README file"
     readme_content = fp_readme.read_text()
-    assert readme_content.startswith(f'# {project_name}'), "README should start with the project name"
-    assert "## Installation" in readme_content, "README should have a getting started section"
+    assert readme_content.startswith(
+        f"# {project_name}"
+    ), "README should start with the project name"
+    assert (
+        "## Installation" in readme_content
+    ), "README should have a getting started section"
 
-    fp_changelog = tmp_path / 'CHANGELOG.md'
+    fp_changelog = tmp_path / "CHANGELOG.md"
     assert fp_changelog.is_file(), "new projects should have a CHANGELOG file"
 
-    fp_precommit_config = tmp_path / '.pre-commit-config.yaml'
+    fp_precommit_config = tmp_path / ".pre-commit-config.yaml"
     assert fp_precommit_config.is_file() == use_precommit
 
-    fp_bumpversion_config = tmp_path / '.bumpversion.cfg'
+    fp_bumpversion_config = tmp_path / ".bumpversion.cfg"
     assert fp_bumpversion_config.is_file() == use_bumpversion
 
-    fp_git = tmp_path / '.git'
+    fp_git = tmp_path / ".git"
     assert fp_git.is_dir(), "new projects should be git repositories"
 
-    fp_git_config = fp_git / 'config'
+    fp_git_config = fp_git / "config"
     git_config = fp_git_config.read_text()
-    assert '[remote "origin"]' in git_config, "new projects should have a remote repository configured"
+    assert (
+        '[remote "origin"]' in git_config
+    ), "new projects should have a remote repository configured"
+
+    os.chdir(tmp_path)
+    check_output(["git", "add", "."])
+    check_output(["git", "commit", "-m", "initial commit"])
+
+    # verify that example can be installed
+    venv.install(".[doc,dev,test]", editable=True)
+    venv_bin = Path(venv.bin)
+
+    # verify that pytest works and all tests pass
+    check_output([venv_bin / "pytest", "-q"])
